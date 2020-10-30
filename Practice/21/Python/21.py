724418f9ea5b7f3c08cb5fdd67e816add4594342
terrepta <koroteevna@gmail.com>
2020-10-30 11:10:14 +0300

    fresh commit

--- Practice/21/Python/21.py
+++ Practice/21/Python/21.py
@@ -1,15 +1,14 @@
 
 def bmi(weight:float, height:float):
     height = height / 100
-    BMI = weight / (height * height)
-    return BMI
+    bmi = weight / (height * height)
+    return bmi
 
 def print_bmi(bmi:float):
-    bmi(weight, height)
-    if (BMI < 18.5): print("Underweight")
-    elif (BMI >= 18.5 and BMI < 25.0): print("Normal weight")
-    elif (BMI >= 25.0 and BMI < 30.0): print("Overweight")
-    elif (30.0 <= BMI): print("Obesity")
+    if (bmi < 18.5): print("Underweight")
+    elif (bmi >= 18.5 and bmi < 25.0): print("Normal weight")
+    elif (bmi >= 25.0 and bmi < 30.0): print("Overweight")
+    elif (30.0 <= bmi): print("Obesity")
 
 weight,height = map(float, input("Put your weight and height\n").split())
 print_bmi(bmi(weight, height))
