2e08ad3e5557f4d083fa5c6e37159f5250682091
terrepta <koroteevna@gmail.com>
2020-11-17 22:00:05 +0300

    changed code

--- Practice/24/C++/24/24.cpp
+++ Practice/24/C++/24/24.cpp
@@ -11,33 +11,27 @@ int main()
 	json in_file;	//данные из файла in.json
 	json out_file = json::array();		//данные, которые будут записаны в out.json
 	json temporary_storage;		//временное хранилище данных
-
 	ifstream read("in.json");			//чтение файла in.json
 	read >> in_file;
-	int sum_of_users = 0;
-	int* number_of_completed_tasks = new int [sum_of_users] { 0 };			//создание динамического массива c кол-вом элементов равном кол-ву юзеров
+	int sum_of_users = 0;		
 
-	for (int i = 0; i < in_file.size(); i++) {
-		if (in_file[i]["userId"] >= sum_of_users) {
+	for (int i = 0; i < in_file.size(); i++) {		//подсчет количества юзеров
+		if (in_file[i]["userId"] >= sum_of_users)
 			sum_of_users = in_file[i]["userId"];
-		}
-		for (int u = 1; u <= sum_of_users; u++)
+	}
+	int* number_of_completed_tasks = new int [sum_of_users] { 0 };		//создание динамического массива c кол-вом элементов равном кол-ву юзеров
+	for (int i = 0; i < in_file.size(); i++) {							//условие, проверяющее каждый элемент in.json
+		for (int u = 1; u <= sum_of_users; u++)							//условие, проверяющее кадого юзера по очереди
 		{
 			if (in_file[i]["completed"] == true && in_file[i]["userId"] == u)
-			{
-				number_of_completed_tasks[u-1]++;		//подсчет выполненных заданий каждого юзера
-			}
+				number_of_completed_tasks[u-1]++;						//подсчет количества выполненных заданий для каждого юзера
 		}
 	}
-
-	for (int i = 1; i <= sum_of_users; i++) {
-		temporary_storage[i]["task_completed"] = number_of_completed_tasks[i-1];	//внесение данных о количестве выполненных заданий в переменную временного хранения temporary_storage
+	for (int i = 1; i <= sum_of_users; i++) {							//заносим все данные во временное хранилище данных temporary_storage
+		temporary_storage[i]["task_completed"] = number_of_completed_tasks[i-1];
 		temporary_storage[i]["userId"] = i;
 	}
-	out_file.push_back(temporary_storage); //загрузка полученных данных из temporary_storage в другую переменную
-	ofstream add("out.json");
-	add << setw(2) << out_file;		//добавление данных из out_file в файл out.json
+	out_file.push_back(temporary_storage);			//добавление элементов в массив 
+	ofstream add("out.json");		
+	add << setw(2) << out_file;		//запись данных из out_file в файл out.json
 	}
-
-
-	
